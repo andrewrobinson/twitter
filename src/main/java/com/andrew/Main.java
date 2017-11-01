327272ae535c4e5f380f8dea2e9d1b5d92dd3cf4
Andrew Robinson <andrewmcrobinson@gmail.com>
2017-11-01 22:25:27 +0200

    specify 7-bit ascii

--- src/main/java/com/andrew/Main.java
+++ src/main/java/com/andrew/Main.java
@@ -11,19 +11,20 @@ public class Main {
     public static void main(String[] args) {
 
         if (args.length != 2) {
-            throw new RuntimeException("exactly two commandline params expected, 1:users Filepath and 2:tweets Filepath ");
+            throw new RuntimeException("exactly two commandline params expected, 1:users filepath and 2:tweets filepath ");
         }
 
-        Stream<String> users = readFileAsStream(args[0], "Problem reading from usersFilepath:");
-        Stream<String> allTweets = readFileAsStream(args[1], "Problem reading from tweetsFilepath:");
+        Stream<String> users = readFileAsStream(args[0], "Problem reading from users filepath.");
+        Stream<String> allTweets = readFileAsStream(args[1], "Problem reading from tweets filepath.");
 
-        TwitterParser.showTweets(users, allTweets);
+        System.out.println(TwitterParser.getTweetFeedForDisplay(users, allTweets));
 
     }
 
     private static Stream<String> readFileAsStream(String filename, String errorMessage) {
 
         try {
+            //7-bit ASCII was specified
             return Files.lines(Paths.get(filename), StandardCharsets.US_ASCII);
         } catch (IOException e) {
             throw new RuntimeException(errorMessage, e);
